e0169cb34546ec519384452f0266eb199b9e8f01
Rodrigo Vélez <rodrigovelez84@gmail.com>
2020-09-08 08:53:26 -0500

    Update

---
+++ DI Code.R
@@ -0,0 +1,12 @@
+################################################
+##          DISTRIBUCIÓN INDICES              ##
+################################################
+
+
+################################################
+# Comando para borra el enviroment 
+rm(list=ls())
+
+# Importar
+library(readxl)
+Indices <- read_excel("C:/Users/Rodro/Documents/R/DI/Base.xlsx")
