200947cb679f0ffae679eec9d12b290015a314eb
Rodrigo Vélez <rodrigovelez84@gmail.com>
2020-09-08 14:45:27 -0500

    Update

--- DI Code.R
+++ DI Code.R
@@ -166,3 +166,156 @@ for (k in 1:ncp) {
 
 # Exportar tablas de resumen
 write.table(tlb, file = "tlb.cvs", sep = ",")
+
+### 2.2 Pruebas estadísticas para Estacionariedad
+
+#-- ADF: el max rezago se calcula de acuerdo a [12*(T/100)^(1/4)] donde [] es la parte entera del número
+#-- se realiza un pequeño "algoritmo" donde se mira si el útlimo rezago tiene un abs(t-stat) > 1.6, si no
+#-- se reduce en 1 la cantidad de rezagos en la prueba
+#-- finalmente si no encuentra ningún rezago significativo se deja por default lag = 1
+
+#--- adfp1 sin constante y sin tendencia
+
+# Matriz adfp1: i = filas (fechas), j = submuestras, k = indices (S&P500, COLCAP, IPyC, IPSA y SPBVL)
+adfp1 = array(data = NA, dim = c(n1,n_sm,ncp))
+dimnames(adfp1)[[2]] = c("S-60","S-250","S-500","S-1000")
+dimnames(adfp1)[[3]] = c("S&P500","COLCAP","IPyC","IPSA","SPBVL")
+
+for (k in 1:ncp) {
+  for (j in 1:n_sm) {
+    for(i in sm[j]:(n1-1)) {
+      
+      tmp = (i-sm[j]+1)
+      tl = trunc(12*((sm[j]/100)^0.25))
+      
+      for(z in 0:(tl-1)) {
+        
+        a = adfTest(r[tmp:i,k], lags = (tl - z), type = "nc" )
+        tts = summary(a@test$lm)[["coefficients"]][, "t value"]
+        if (abs(tts[length(tts)]) > 1.6) {
+          adfp1[i,j,k] = a@test$p.value
+          break
+          
+        }
+        if ((tl-z)==1){
+          adfp1[i,j,k] = a@test$p.value
+        }
+      }
+    }
+  }
+}
+
+## Array para resumir resultados de todos los indicies adf-nc
+
+tadfnc = array(data = NA, dim = c(n_sm,2,ncp))
+dimnames(tadfnc)[[1]] = c("60","250","500","1000")
+dimnames(tadfnc)[[2]] = c("Rechazo","No Rechazo")
+dimnames(tadfnc)[[3]] = c("S&P500","COLCAP","IPyC","IPSA","SPBVL")
+
+for (k in 1:ncp) {
+  for (i in 1:n_sm) {
+    
+    tadfnc[i,1,k] = sum(adfp1[,i,k]<0.05, na.rm = TRUE)
+    tadfnc[i,2,k] = sum(adfp1[,i,k]>=0.05, na.rm = TRUE)
+    
+  }
+}
+
+#--- adfp2 con constante y sin tendencia
+
+# Matriz adfp2: i = filas (fechas), j = submuestras, k = indices (S&P500, COLCAP, IPyC, IPSA y SPBVL)
+adfp2 = array(data = NA, dim = c(n1,n_sm,ncp))
+dimnames(adfp2)[[2]] = c("S-60","S-250","S-500","S-1000")
+dimnames(adfp2)[[3]] = c("S&P500","COLCAP","IPyC","IPSA","SPBVL")
+
+for (k in 1:ncp) {
+  for (j in 1:n_sm) {
+    for(i in sm[j]:(n1-1)) {
+      
+      tmp = (i-sm[j]+1)
+      tl = trunc(12*((sm[j]/100)^0.25))
+      
+      for(z in 0:(tl-1)) {
+        
+        a = adfTest(r[tmp:i,k], lags = (tl - z), type = "c" )
+        tts = summary(a@test$lm)[["coefficients"]][, "t value"]
+        if (abs(tts[length(tts)]) > 1.6) {
+          adfp2[i,j,k] = a@test$p.value
+          break
+          
+        }
+        if ((tl-z)==1){
+          adfp2[i,j,k] = a@test$p.value
+        }
+      }
+    }
+  }
+}
+
+## Array para resumir resultados de todos los indicies adf-c
+
+tadfc = array(data = NA, dim = c(n_sm,2,ncp))
+dimnames(tadfc)[[1]] = c("60","250","500","1000")
+dimnames(tadfc)[[2]] = c("Rechazo","No Rechazo")
+dimnames(tadfc)[[3]] = c("S&P500","COLCAP","IPyC","IPSA","SPBVL")
+
+for (k in 1:ncp) {
+  for (i in 1:n_sm) {
+    
+    tadfc[i,1,k] = sum(adfp2[,i,k]<0.05, na.rm = TRUE)
+    tadfc[i,2,k] = sum(adfp2[,i,k]>=0.05, na.rm = TRUE)
+    
+  }
+}
+
+#--- adfp3 con constante y con tendencia
+# Matriz adfp3: i = filas (fechas), j = submuestras, k = indices (S&P500, COLCAP, IPyC, IPSA y SPBVL)
+adfp3 = array(data = NA, dim = c(n1,n_sm,ncp))
+dimnames(adfp3)[[2]] = c("S-60","S-250","S-500","S-1000")
+dimnames(adfp3)[[3]] = c("S&P500","COLCAP","IPyC","IPSA","SPBVL")
+
+for (k in 1:ncp) {
+  for (j in 1:n_sm) {
+    for(i in sm[j]:(n1-1)) {
+      
+      tmp = (i-sm[j]+1)
+      tl = trunc(12*((sm[j]/100)^0.25))
+      
+      for(z in 0:(tl-1)) {
+        
+        a = adfTest(r[tmp:i,k], lags = (tl - z), type = "ct" )
+        tts = summary(a@test$lm)[["coefficients"]][, "t value"]
+        if (abs(tts[length(tts)]) > 1.6) {
+          adfp3[i,j,k] = a@test$p.value
+          break
+          
+        }
+        if ((tl-z)==1){
+          adfp3[i,j,k] = a@test$p.value
+        }
+      }
+    }
+  }
+}
+
+## Array para resumir resultados de todos los indicies adf-tc
+
+tadftc = array(data = NA, dim = c(n_sm,2,ncp))
+dimnames(tadftc)[[1]] = c("60","250","500","1000")
+dimnames(tadftc)[[2]] = c("Rechazo","No Rechazo")
+dimnames(tadftc)[[3]] = c("S&P500","COLCAP","IPyC","IPSA","SPBVL")
+
+for (k in 1:ncp) {
+  for (i in 1:n_sm) {
+    
+    tadftc[i,1,k] = sum(adfp3[,i,k]<0.05, na.rm = TRUE)
+    tadftc[i,2,k] = sum(adfp3[,i,k]>=0.05, na.rm = TRUE)
+    
+  }
+}
+
+# Exportar tablas de resumen
+write.table(tadfnc, file = "tadfnc.cvs", sep = ",")
+getwd()# direccion donde quedó el archivo 
+write.table(tadfc, file = "tadfc.cvs", sep = ",")
+write.table(tadftc, file = "tadftc.cvs", sep = ",")
